6d1e365af944361a5ef442016b71559d62319247
Ilias Elbadaoui <ielbadao@e2r1p12.1337.ma>
2021-12-06 17:02:27 +0100

    CAPACITY METHODS ONGONG

--- vector.hpp
+++ vector.hpp
@@ -1,11 +1,20 @@
 namespace ft
 {
 	# include <memory>
+	# include <exception>
 	# include "iterator_traits.hpp"
 	# define X64_MAX 184467440737095.51616e5
 	template< class T,  class Allocator=std::allocator<T> >
 	class vector
 	{
+		struct length_error : public std::exception
+		{
+			const char * what () const throw ()
+			{
+				return "length error exception";
+			}
+		};
+
 		typedef 		 T									value_type;
         typedef 		 Allocator                          allocator_type;
 		typedef	typename allocator_type::pointer			pointer;
@@ -15,73 +24,74 @@ namespace ft
 		typedef 		 size_t								size_type;
 		// typedef			 std::ptrdiff_t						difference_type
         
-        pointer     	array;
-        size_type       capacity;
+        pointer     	_array;
+        size_type       _capacity;
         size_type       _size;
-        allocator_type	alloc;
+        allocator_type	_alloc;
 
-		void destroySelf()
+		void destroySelf(size_type start, size_type end)
 		{
-			for (size_type i = 0; i < this->_size; i++)
-					this->alloc.destroy(this->array + i);
-				this->alloc.deallocate(this->array, this->capacity);
+			for (size_type i = start; i < end; i++)
+					this->_alloc.destroy(this->_array + i);
+			if (end - start == this->_capacity)
+				this->_alloc.deallocate(this->_array, this->_capacity);
 		}
 	public:
 
 		// CONSTRUCTORS
 		explicit vector(const allocator_type& alloc = allocator_type())
 		{
-            this->array = NULL;
-            this->capacity = 0;
+            this->_array = NULL;
+            this->_capacity = 0;
             this->_size = 0;
-			this->alloc = alloc;
+			this->_alloc = alloc;
 		}
 		explicit vector(size_type n, const value_type& val = value_type(),
                 	const allocator_type& alloc = allocator_type())
 		{
-			this->alloc = alloc;
-            this->array = this->alloc.allocate(n);
-            this->capacity = n;
+			this->_alloc = alloc;
+            this->_array = this->_alloc.allocate(n);
+            this->_capacity = n;
             this->_size = n;
 			for (size_type i = 0; i < n; i++)
-				this->alloc.construct(this->array + i, val);
+				this->_alloc.construct(this->_array + i, val);
 		}
 		vector (const vector& x)
 		{
-			this->alloc = x.alloc;
-			this->capacity = x.capacity;
+			this->_alloc = x.alloc;
+			this->_capacity = x._capacity;
 			this->_size = this->_size;
-			this->array = this->alloc.allocate(this->capacity);
+			this->_array = this->_alloc.allocate(this->_capacity);
 			for (size_type i = 0; i < this->_size; i++)
-				this->alloc.construct(this->array + i, x.array[i]);
+				this->_alloc.construct(this->_array + i, x._array[i]);
 		}
 		// CONSTRUCTORS END
 
 		// OPERATORS
 		vector& operator= (const vector& x)
 		{
-			if (this->array)
-				destroySelf();
-			this->alloc = x.alloc;
-			this->capacity = x.capacity;
+			if (this->_array)
+				destroySelf(0, this->_capacity);
+			this->_alloc = x.alloc;
+			this->_capacity = x._capacity;
 			this->_size = this->_size;
-			this->array = this->alloc.allocate(this->capacity);
+			this->_array = this->_alloc.allocate(this->_capacity);
 			for (size_type i = 0; i < this->_size; i++)
-				this->alloc.construct(this->array + i, x.array[i]);
+				this->_alloc.construct(this->_array + i, x._array[i]);
 			return *this;
 		}
 
 		reference operator[] (size_type n)
 		{
-			return this->array[n];
+			return this->_array[n];
 		}
 		const_reference operator[] (size_type n) const
 		{
-			return this->array[n];
+			return this->_array[n];
 		}
 		// OPERATORS END
 
-		// CAPACITY METHODS
+		// _CAPACITY METHODS
 		
 		// SIZE
 		size_type size() const
@@ -103,16 +113,67 @@ namespace ft
 		// RESIZE
 		void resize (size_type n, value_type val = value_type())
 		{
-
+			if (n < this->_size)
+			{
+				destroySelf(n, this->_size - 1);
+			}
+			else
+			{
+				pointer		tmp;
+				tmp = this->_alloc.allocate(n);
+				for (size_type i = 0; i < this->_size; i++)
+					tmp[i] = this->_array[i];
+				for (size_type i = this->_size; i < n; i++)
+					tmp[i] = val;
+				destroySelf(0, this->_capacity);
+				this->_array = tmp;
+			}
+			this->_size = n;
+			this->_capacity = n;
 		}
 		// RESIZE END
 
+		// CAPACITY
+		size_type capacity() const
+		{
+			return this->_capacity;
+		}
+		// CAPACITY END
+
+		// EMPTY
+		bool empty() const
+		{
+			if (this->_size == 0)
+				return true;
+			return false;
+		}
+		// EMPTY END
+
+		// RESERVE
+		void reserve (size_type n)
+		{
+			if (n > max_size())
+				throw length_error();
+			if (n > this->_capacity)
+			{
+				pointer		tmp;
+				tmp = this->_alloc.allocate(n);
+				for (size_type i = 0; i < this->_size; i++)
+					tmp[i] = this->_array[i];
+				destroySelf(0, this->_capacity);
+				this->_array = tmp;
+			}
+			this->_capacity = n;
+		}
+		// RESERVE END
 		// CAPACITY METHODS END
 
+		// ELEMENT ACCESS METHODS
+		// ELEMENT ACCESS METHODS END
 		// DESTRUCTOR
 		~vector()
 		{
-			destroySelf();
+			destroySelf(0, this->_capacity);
 		}
 		// DESTRUCTOR END
 	};
--- vector_main.cpp
+++ vector_main.cpp
@@ -5,13 +5,40 @@
 using namespace std;
 int main ()
 {
-    typedef    char     type;
-    std::vector<type> std_vect(5, 'X');
-    ft::vector<type> ft_vect(5, 'X');
+    typedef    int     type;
+    std::vector<type> std_vect(5, 101);
+    ft::vector<type> ft_vect(5, 101);
     
     cout << "STD [0] operator : " << std_vect[0] << endl;
     cout << "FT [0] operator : " << ft_vect[0] << endl;
     cout << "STD max_size : " << std_vect.max_size() << endl;
     cout << "FT max_size : " << ft_vect.max_size() << endl;
+
+    cout << "------------------" << endl;
+    cout << "AFTER RESIZE" << endl;
+    cout << "------------------" << endl;
+    std_vect.resize(10, 11);
+    ft_vect.resize(10, 11);
+    cout << "STD [0] operator : " << std_vect[0] << endl;
+    cout << "FT [0] operator : " << ft_vect[0] << endl;
+    cout << "STD [6] operator : " << std_vect[6] << endl;
+    cout << "FT [6] operator : " << ft_vect[6] << endl;
+    cout << "STD max_size : " << std_vect.max_size() << endl;
+    cout << "FT max_size : " << ft_vect.max_size() << endl;
+
+    cout << "------------------" << endl;
+    cout << "EMPTY METHOD" << endl;
+    cout << "------------------" << endl;
+    cout << "STD EMPTY : " << std_vect.empty() << endl;
+    cout << "FT EMPTY : " << ft_vect.empty() << endl;
+    cout << "------------------" << endl;
+    cout << "RESERVE METHOD" << endl;
+    cout << "------------------" << endl;
+    std_vect.reserve(12);
+    ft_vect.reserve(12);
+    cout << "STD [11] operator : " << std_vect[11] << endl;
+    cout << "FT [11] operator : " << ft_vect[11] << endl;
+    cout << "STD [6] operator : " << std_vect[6] << endl;
+    cout << "FT [6] operator : " << ft_vect[6] << endl;
     return 0;
 }
